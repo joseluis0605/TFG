e3d3cfd86c5c9de03730cb02f09a0622a844e29d
José Luis <joseluis06052001@gmail.com>
2023-09-15 13:18:01 +0200

    cargador solo lectura y mostrar

--- src/com/company/CargadorFile.java
+++ src/com/company/CargadorFile.java
@@ -9,29 +9,29 @@ public class CargadorFile {
 
     public CargadorFile(String nombre){
         String ruta="C:\\Users\\USUARIO\\Desktop\\URJC\\4º Ciberseguridad\\TFG\\AlphaSeparatorProblem\\src\\com\\company\\pruebasFiles\\"+nombre;
-        this.path=ruta;
+        this.path=ruta; //tenemos la ruta + el nombre del archivo .txt
     }
 
     public void leerFile(){
-        Scanner entrada= null;
-        File fichero= new File(this.path);
+
+        Scanner entrada= null; // puntero tipo Scanner apuntando a null (inicializar)
+        File fichero= new File(this.path); //abrimos el fichero cuya ruta esta en path
         try {
-            entrada= new Scanner(fichero);
-            while (entrada.hasNextLine()){
-                String frase = entrada.nextLine();
-                System.out.println(frase);
+            entrada= new Scanner(fichero); //recogemos datos del FICHERO y lo guardamos en entrada
+            while (entrada.hasNextLine()){ //mientras el fichero no termine
+                String frase = entrada.nextLine(); //recogemos la frase completa
+                System.out.println(frase); //imprimimos
             }
         }catch (Exception exception){
-            System.out.println(exception.getMessage());
+            System.out.println(exception.getMessage()); //imprimir mensaje de error si hay fallo en el TRY
         }finally {
             try {
                 if (entrada!=null) {
-                    entrada.close();
+                    entrada.close(); //cerramos el fichero
                 }
             }catch (Exception exception){
                 System.out.println(exception.getMessage());
             }
             }
-
     }
 }
