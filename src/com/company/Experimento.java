22ea3a94e61ff498d135be125087fbfa11e305be
José Luis <joseluis06052001@gmail.com>
2023-09-23 16:53:25 +0200

    ajustes creando clases separadoras de funciones

--- src/com/company/Experimento.java
+++ src/com/company/Experimento.java
@@ -1,10 +1,16 @@
 package com.company;
 
+import java.util.List;
+
 public class Experimento {
 
 /*
     En esta clase vamos a realizar los experimentos, es decir, va a ser donde se llame a la ejecucion de los algoritmos
  */
 
+    public static void main(String[] args) {
 
+        List<String> listado= FileNameList.getFileNameList();
+        System.out.println(listado.size());
+    }
 }
