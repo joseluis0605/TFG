85bd6a3a8dbb33841ebe161ba772a09bbf4b6b83
José Luis <joseluis06052001@gmail.com>
2023-11-28 12:32:04 +0100

    COMPROBACION FICHEROS

--- src/test/java/com/github/joseluis0605/Pruebas.java
+++ src/test/java/com/github/joseluis0605/Pruebas.java
@@ -2,14 +2,72 @@ package com.github.joseluis0605;
 
 import com.github.joseluis0605.TFG_CODIGO.CARGADORES.*;
 import com.github.joseluis0605.TFG_CODIGO.CONSTRUCTIVOS.MejoraSolucion;
+import com.github.joseluis0605.TFG_CODIGO.FICHEROS.FileNameList;
 import com.github.joseluis0605.TFG_CODIGO.INSTANCIA.Instancia;
 import com.github.joseluis0605.TFG_CODIGO.INSTANCIA.Solucion;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Pruebas {
+
+    @Test
+    public void comprobarComponentes() throws FileNotFoundException {
+
+        List<String> listadoNoComponentesConexas= new ArrayList<>();
+        listadoNoComponentesConexas.add("erdos_renyi_100_0.05_0.2_0.txt");
+        listadoNoComponentesConexas.add("erdos_renyi_100_0.05_0.2_2.txt");
+        listadoNoComponentesConexas.add("erdos_renyi_100_0.05_0.5_0.txt");
+        listadoNoComponentesConexas.add("erdos_renyi_100_0.06_0.8_4.txt");
+
+        String ruta= "instanciasExperimentoSergio.txt";
+        Scanner entrada= null; // puntero tipo Scanner apuntando a null (inicializar)
+        File fichero= new File(ruta); //abrimos el fichero cuya ruta esta en path
+        entrada= new Scanner(fichero);
+        List<String> listadoAristas= new ArrayList<>();
+        while (entrada.hasNextLine()){ //mientras el fichero no termine
+            String frase = entrada.nextLine(); //recogemos la frase completa
+            listadoAristas.add(frase);
+        }
+
+        System.out.println("listado de sergio");
+        for (String nombre: listadoAristas){
+            if (listadoNoComponentesConexas.contains(nombre)){
+                System.out.println("esta-->"+nombre);
+            }
+        }
+    }
+
+    @Test
+    public void comprobarContrario(){
+        List<String> listadoFicheros= FileNameList.getFileNameList();
+        String ruta= "instanciasExperimentoSergio.txt";
+        Scanner entrada= null; // puntero tipo Scanner apuntando a null (inicializar)
+        File fichero= new File(ruta); //abrimos el fichero cuya ruta esta en path
+        try {
+            entrada= new Scanner(fichero);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<String> listadoAristas= new ArrayList<>();
+        while (entrada.hasNextLine()){ //mientras el fichero no termine
+            String frase = entrada.nextLine(); //recogemos la frase completa
+            listadoAristas.add(frase);
+        }
+        System.out.println("tam sergio: "+listadoAristas.size());
+
+        for (String nombre: listadoFicheros){
+            if (!listadoAristas.contains(nombre)){
+                System.out.println("no contiene-->"+nombre);
+            }
+        }
+        System.out.println("tam-->"+listadoFicheros.size());
+    }
 
     @Test
     public void BT(){
@@ -61,5 +119,7 @@ public class Pruebas {
         CargadorExperimento cargadorExperimento= new CargadorExperimento3();
         cargadorExperimento.cargarExperimento();
     }
+
+
 
 }
